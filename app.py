e189703851c8ed658ef670376ce4804cd8e15da2
gabesolomon10 <gsolo@sas.upenn.edu>
2019-12-14 12:34:24 -0800

    push1

--- app.py
+++ app.py
@@ -50,18 +50,18 @@ def home():
 
 	#Add necessary columns to standings
 	standings_df = western_conf_df.append(eastern_conf_df)
-	
+
 	standings_df['Team'] = standings_df['Team'].str.extract('(.*)[(]')
 	standings_df['Team'] = standings_df['Team'].astype(str)
 	standings_df['Team'] = standings_df['Team'].str.strip()
 
 	standings_df["Record"] = standings_df['W'].map(str) + "-" + standings_df['L'].map(str)
 
-
-
 	# Set up the teams
-	teams = pd.DataFrame({'Team Name': ['Team Nebeyu', 'They all start with 0 wins', 
-	                                    "Our friend is an alcoholic and it's troubling", "Sammy Ps AF1s Est. 2011",
+	teams = pd.DataFrame({'Team Name': ['Team Nebeyu', 
+										'They all start with 0 wins', 
+	                                    "Our friend is an alcoholic and it's troubling", 
+	                                    "Sammy Ps AF1s Est. 2011",
 	                                    "Pre-pubescent toddler",
 	                                    "Did it",
 	                                    "Benjamin Bogdanovic"],
@@ -73,7 +73,7 @@ def home():
 									os.path.join(app.config['profiles_folder'],'young.png'),
 									os.path.join(app.config['profiles_folder'],'ben.png')],
 	                      'Team 1': ['Milwaukee Bucks','Philadelphia 76ers', 'Los Angeles Clippers',
-                                     'Houston Rockets','Denver Nuggets', 'Los Angeles Lakers', ' Boston Celtics'],
+	                      			  'Houston Rockets','Denver Nuggets', 'Los Angeles Lakers', ' Boston Celtics'],
 	                      'Team 2': ['San Antonio Spurs', 'Portland Trail Blazers', 'Brooklyn Nets',
                                      'Indiana Pacers', 'Toronto Raptors', 'Golden State Warriors', 'Utah Jazz'],
 	                      'Team 3': ['Miami Heat', 'Dallas Mavericks', 'Orlando Magic',
@@ -83,6 +83,7 @@ def home():
 
 	#Create the wins table
 	merged_wins = teams.merge(standings_df, left_on='Team 1', right_on='Team')
+
 	merged_wins = merged_wins[['Team Name', 'Owner', 'Team 1', 'W', 'L', 'Record', 'Team 2', 'Team 3', 'Team 4']]
 	merged_wins = merged_wins.rename(columns={"W": 'Team 1 Wins', 'L': 'Team 1 Losses', "Record": "Team 1 Record"})
 
@@ -104,7 +105,8 @@ def home():
 							   'Team 4', 'W', 'L', 'Record']]
 	merged_wins = merged_wins.rename(columns={"W": 'Team 4 Wins', 'L': 'Team 4 Losses', "Record": "Team 4 Record"})
 
-	merged_wins = merged_wins[['Team Name','Owner', 'Team 1', 'Team 1 Wins', 'Team 1 Losses', 'Team 1 Record',
+	merged_wins = merged_wins[['Team Name','Owner', 
+	'Team 1', 'Team 1 Wins', 'Team 1 Losses', 'Team 1 Record',
 							   'Team 2', 'Team 2 Wins', 'Team 2 Losses', 'Team 2 Record',
 							   'Team 3', 'Team 3 Wins', 'Team 3 Losses', 'Team 3 Record',
 							   'Team 4', 'Team 4 Wins', 'Team 4 Losses', 'Team 4 Record']]
@@ -119,41 +121,41 @@ def home():
 
 	merged_wins = merged_wins[['Team Name','Owner', 'Total Wins', 'Win Percentage',
 							   'Team 1', 'Team 1 Record',
-							   'Team 2','Team 2 Record',
+							   'Team 2', 'Team 2 Record',
 							   'Team 3', 'Team 3 Record',
 							   'Team 4',  'Team 4 Record']]
 	#NBA Logos
-	merged_wins['Team 1 Image'] = [os.path.join(app.config['nba_folder'],'bucks.png'),
+	merged_wins['Team 1 Image'] = pd.Series([os.path.join(app.config['nba_folder'],'bucks.png'),
 								   os.path.join(app.config['nba_folder'],'76ers.png'),
 								   os.path.join(app.config['nba_folder'],'clippers.png'),
 								   os.path.join(app.config['nba_folder'],'rockets.png'),
 								   os.path.join(app.config['nba_folder'],'nuggets.png'),
 								   os.path.join(app.config['nba_folder'],'lakers.png'),
-								   os.path.join(app.config['nba_folder'],'celtics.png')]
+								   os.path.join(app.config['nba_folder'],'celtics.png')])
 
-	merged_wins['Team 2 Image'] = [os.path.join(app.config['nba_folder'],'spurs.png'),
+	merged_wins['Team 2 Image'] = pd.Series([os.path.join(app.config['nba_folder'],'spurs.png'),
 								   os.path.join(app.config['nba_folder'],'trail_blazers.png'),
 								   os.path.join(app.config['nba_folder'],'nets.png'),
 								   os.path.join(app.config['nba_folder'],'pacers.png'),
 								   os.path.join(app.config['nba_folder'],'raptors.png'),
 								   os.path.join(app.config['nba_folder'],'warriors.png'),
-								   os.path.join(app.config['nba_folder'],'jazz.png')]
+								   os.path.join(app.config['nba_folder'],'jazz.png')])
 
-	merged_wins['Team 3 Image'] = [os.path.join(app.config['nba_folder'],'heat.png'),
+	merged_wins['Team 3 Image'] = pd.Series([os.path.join(app.config['nba_folder'],'heat.png'),
 								   os.path.join(app.config['nba_folder'],'mavericks.png'),
 								   os.path.join(app.config['nba_folder'],'magic.png'),
 								   os.path.join(app.config['nba_folder'],'pelicans.png'),
 								   os.path.join(app.config['nba_folder'],'timberwolves.png'),
 								   os.path.join(app.config['nba_folder'],'pistons.png'),
-								   os.path.join(app.config['nba_folder'],'hawks.png')]
+								   os.path.join(app.config['nba_folder'],'hawks.png')])
 
-	merged_wins['Team 4 Image'] = [os.path.join(app.config['nba_folder'],'grizzlies.png'),
+	merged_wins['Team 4 Image'] = pd.Series([os.path.join(app.config['nba_folder'],'grizzlies.png'),
 								   os.path.join(app.config['nba_folder'],'knicks.png'),
 								   os.path.join(app.config['nba_folder'],'wizards.png'),
 								   os.path.join(app.config['nba_folder'],'suns.png'),
 								   os.path.join(app.config['nba_folder'],'thunder.png'),
 								   os.path.join(app.config['nba_folder'],'bulls.png'),
-								   os.path.join(app.config['nba_folder'],'kings.png')]
+								   os.path.join(app.config['nba_folder'],'kings.png')])
 
 	merged_wins = merged_wins.sort_values(by=['Total Wins'], ascending = False)
 	merged_wins.reset_index(drop=True, inplace=True)
@@ -163,8 +165,8 @@ def home():
 	                                       team3_data = merged_wins.iloc[2].values, 
 	                                       team4_data = merged_wins.iloc[3].values,
 	                                       team5_data = merged_wins.iloc[4].values, 
-	                                       team6_data = merged_wins.iloc[5].values,
-	                                       team7_data = merged_wins.iloc[6].values,)
+	                                       team6_data = merged_wins.iloc[5].values)
+	                                       #team7_data = merged_wins.iloc[6].values,)
 
 if __name__ == "__main__":
     app.run(debug=True)
